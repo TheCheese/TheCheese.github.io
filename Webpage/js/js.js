1ffba9962ba9a399eb68cf353bb3e9a2586b3d81
TheCheese <alexchers@gmail.com>
2016-05-13 21:41:57 +1000

    Add files via upload
    slider js code added

--- Webpage/js/js.js
+++ Webpage/js/js.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
+    
+    $("#navbarBottom").hide();
     $("#navbarTopLi1").click(function() {
         $("#navbarBottom").toggle();
-    })
-    $("#navbarBottom").hide();
+    });
+    
+$(function(){
+   function slideSwitch(){
+       var $active = $('#slideshow img.active');
+       var $next = $active.next();
+
+        if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+
+        var $next =  $active.next().length ? $active.next()
+        : $('#slideshow IMG:first');
+       
+       $active.addClass('last-active');
+
+       $next.css({opacity: 0.0})
+         .addClass('active')
+         .animate({opacity:1.0}, 1000, function() {
+              $active.removeClass('active last-active');
+         });
+  }
+  setInterval(slideSwitch, 5000 );
+});
 });
